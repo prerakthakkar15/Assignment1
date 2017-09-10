31eb92b6eebe3bc25f7ac4fc5d66fd592fdea3cf
Prerak Thakkar <Prerak@preraks-mac-mini-2.local>
2017-09-10 14:21:52 +0530

    Initial Commit

---
+++ pterakTestAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pterakTestAssignment
+//
+//  Created by Prerak Thakkar on 10/09/17.
+//  Copyright © 2017 Prerak Thakkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ pterakTestAssignment/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pterakTestAssignment
+//
+//  Created by Prerak Thakkar on 10/09/17.
+//  Copyright © 2017 Prerak Thakkar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
